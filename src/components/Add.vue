0942c5a8b401eb791fc347d1c45882956d8af620
Mingtao Li <mingtao@xjtu.edu.cn>
2022-09-15 09:32:03 +0800

    改进尝试vue

--- src/components/Add.vue
+++ src/components/Add.vue
@@ -1,11 +1,50 @@
+<script lang="ts" setup>
+  import { ref } from 'vue';
+  import {
+    Check,
+    Delete,
+    Edit,
+    Message,
+    Search,
+    Star,
+  } from '@element-plus/icons-vue';
+import { controlledComputed } from '@vueuse/shared';
+import axios from 'axios';
+  
+  const answer = ref(0);
+  const remoteanswer = ref(1);
+function getAnswer() {
+
+    answer.value++
+
+}
+
+function getAnswerremote() {
+  const res=  axios.get('/api/addxy', {
+    params: {
+            x: 2,
+            y: 3
+          }
+        });
+
+ //这块怎么写？搞不定了，julia端显示已经请求成功，这边无法搞出来
+}
+
+
+
+
+
+  </script>
 <template>
     <el-row class="mb-4">
-      <el-button @click="getAnswer">计算测试</el-button>
-      <el-button type="primary">Primary</el-button>
-      <el-button type="success">Success</el-button>
-      <el-button type="info">Info</el-button>
-      <el-button type="warning">Warning</el-button>
-      <el-button type="danger">Danger</el-button>
+      <el-button @click="getAnswer">本地计算测试</el-button>
+
+      <el-button type="primary">{{answer}}</el-button>
+
+      <el-button @click="getAnswerremote">远程计算测试</el-button>
+
+      <el-button type="primary">{{remoteanswer}}</el-button>
+
     </el-row>
   
     <el-row class="mb-4">
@@ -16,48 +55,6 @@
       <el-button type="warning" plain>Warning</el-button>
       <el-button type="danger" plain>Danger</el-button>
     </el-row>
-  
-    <el-row class="mb-4">
-      <el-button round>Round</el-button>
-      <el-button type="primary" round>Primary</el-button>
-      <el-button type="success" round>Success</el-button>
-      <el-button type="info" round>Info</el-button>
-      <el-button type="warning" round>Warning</el-button>
-      <el-button type="danger" round>Danger</el-button>
-    </el-row>
-  
-    <el-row>
-      <el-button :icon="Search" circle />
-      <el-button type="primary" :icon="Edit" circle />
-      <el-button type="success" :icon="Check" circle />
-      <el-button type="info" :icon="Message" circle />
-      <el-button type="warning" :icon="Star" circle />
-      <el-button type="danger" :icon="Delete" circle />
-    </el-row>
+ 
   </template>
   
-  <script lang="ts" setup>
-  import {
-    Check,
-    Delete,
-    Edit,
-    Message,
-    Search,
-    Star,
-  } from '@element-plus/icons-vue'
-  answer = ref(null);
-//   async getAnswer() {
-//       try {
-//         const res = await axios.get('/api/addxy', {
-//           params: {
-//             x: 2,
-//             y: 3
-//           }
-//         })
-//         answer = res.data.answer
-//       } catch (err) {
-//         alert(err)
-//       }
-//     }
-
-  </script>
--- vite.config.ts
+++ vite.config.ts
@@ -16,7 +16,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "http://127.0.0.1:8080/api",
+        target: "http://127.0.0.1:8000/api",
         ws: true,
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
